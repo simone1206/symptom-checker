ede7023b47336f312c93ad2bb682c5a8fc320fda
Simone Gerner <simone.gerner@stud.hawk.de>
2022-12-21 17:35:23 +0100

    Buttons bei Körperteilauswahl angepasst

--- symptom-checker.js
+++ symptom-checker.js
@@ -3,6 +3,15 @@ function show_bodypart(id) {
     // optional: switch case mit variablen und einem veränderbaren link ganz unten
     // next step: anstelle von buttons koordinatenfelder definieren
 
+
+    //andere Buttons nicht mehr markiert
+    //https://www.w3schools.com/howto/howto_js_tabs.asp am 21.12.22
+    let temp_deactive = document.getElementsByClassName("button_bodypart");
+    for (j = 0; j < temp_deactive.length; j++) {
+        temp_deactive[j].className = temp_deactive[j].className.replace(" active", "");
+    }
+
+
     //andere offene Symptomlisten wieder ausblenden
     //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown am 20.12.22
     let symptoms = document.getElementsByClassName("symptom-content");
@@ -20,46 +29,57 @@ function show_bodypart(id) {
 
             //entsprechende Symptome dazu anzeigen
             document.getElementById('symp_kopf').style.visibility = 'visible';
+
+            //Button markieren
+            event.currentTarget.className += " active";
             break;
 
         case "hals":
             document.getElementById("bild").setAttribute("src", "https://res01.dcmservice.de/widgets/anatomicus/img/anatomicus/man_body_00033.jpg");
             document.getElementById('symp_hals').style.visibility = 'visible';
+            event.currentTarget.className += " active";
             break;
 
         case "arm":
             document.getElementById("bild").setAttribute("src", "https://res01.dcmservice.de/widgets/anatomicus/img/anatomicus/man_body_00034.jpg");
             document.getElementById('symp_arm').style.visibility = 'visible';
+            event.currentTarget.className += " active";
             break;
 
         case "bauch":
             document.getElementById("bild").setAttribute("src", "https://res01.dcmservice.de/widgets/anatomicus/img/anatomicus/man_body_00035.jpg");
             document.getElementById('symp_bauch').style.visibility = 'visible';
+            event.currentTarget.className += " active";
             break;
 
         case "unterleib":
             document.getElementById("bild").setAttribute("src", "https://res01.dcmservice.de/widgets/anatomicus/img/anatomicus/man_body_00036.jpg");
             document.getElementById('symp_unterleib').style.visibility = 'visible';
+            event.currentTarget.className += " active";
             break;
 
         case "hand":
             document.getElementById("bild").setAttribute("src", "https://res01.dcmservice.de/widgets/anatomicus/img/anatomicus/man_body_00037.jpg");
             document.getElementById('symp_hand').style.visibility = 'visible';
+            event.currentTarget.className += " active";
             break;
 
         case "bein":
             document.getElementById("bild").setAttribute("src", "https://res01.dcmservice.de/widgets/anatomicus/img/anatomicus/man_body_00038.jpg");
             document.getElementById('symp_bein').style.visibility = 'visible';
+            event.currentTarget.className += " active";
             break;
 
         case "fuss":
             document.getElementById("bild").setAttribute("src", "https://res01.dcmservice.de/widgets/anatomicus/img/anatomicus/man_body_00039.jpg");
             document.getElementById('symp_fuss').style.visibility = 'visible';
+            event.currentTarget.className += " active";
             break;
 
         case "haut":
             document.getElementById("bild").setAttribute("src", "https://res01.dcmservice.de/widgets/anatomicus/img/anatomicus/man_body_00031.jpg");
             document.getElementById('symp_haut').style.visibility = 'visible';
+            event.currentTarget.className += " active";
             break;
     }
 
@@ -91,6 +111,12 @@ function show_bodypart(id) {
 //geschlecht wählen
 function select_gender(id) {
 
+    //andere Buttons nicht mehr markiert
+    let temp_deactive = document.getElementsByClassName("button_bodypart");
+    for (j = 0; j < temp_deactive.length; j++) {
+        temp_deactive[j].className = temp_deactive[j].className.replace(" active", "");
+    }
+
     //andere offene Symptomlisten wieder ausblenden
     let symptoms = document.getElementsByClassName("symptom-content");
     let i;
@@ -110,6 +136,12 @@ function select_gender(id) {
 
 // körper drehen
 function turn_body() {
+
+    //andere Buttons nicht mehr markiert
+    let temp_deactive = document.getElementsByClassName("button_bodypart");
+    for (j = 0; j < temp_deactive.length; j++) {
+        temp_deactive[j].className = temp_deactive[j].className.replace(" active", "");
+    }
 
     //andere offene Symptomlisten wieder ausblenden
     let symptoms = document.getElementsByClassName("symptom-content");
